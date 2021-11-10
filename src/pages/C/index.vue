d19f37b675fbf1e246693f2cd2595eeb44896cfb
jasonbu <jasonbu@tencent.com>
2021-11-10 10:35:39 +0800

    因为是keep-alive包裹，所以需要使用以下2个钩子去监听或者取消某些事件

--- src/pages/C/index.vue
+++ src/pages/C/index.vue
@@ -3,12 +3,21 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, onActivated, onDeactivated } from 'vue'
 export default {
   name: 'C',
 
   setup() {
     const msg = ref('我是page C')
+
+    // 因为是keep-alive包裹，所以需要使用以下2个钩子去监听或者取消某些事件
+    onActivated(() => {
+      console.log('onActivated');
+    })
+
+    onDeactivated(() => {
+      console.log('onDeactivated');
+    })
 
     return {
       msg
